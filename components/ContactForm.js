e8256527dcb2923870b28b800899d0708f842044
thomasjfcaldwell <thomasjfcaldwell@gmail.com>
2021-12-01 09:59:50 -0800

    contact form done

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -9,29 +9,100 @@ import {
 	InputSubmitContainer,
 	MessageTitle,
 } from './ContactFormStyle';
-
+import { useState } from 'react';
 
 function ContactForm() {
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [phone, setPhone] = useState('');
+	const [message, setMessage] = useState('');
+	const [submitted, setSubmitted] = useState(false);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('sending');
+
+		let data = {
+			name,
+			email,
+			phone,
+			message,
+		};
+		fetch('http://localhost:3000/api/contact', {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json, text/plain, */*',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		}).then((res) => {
+			console.log('Response received');
+			if (res.status === 200) {
+				console.log('Response succeeded!');
+				setSubmitted(true);
+				setName('');
+				setPhone('');
+				setEmail('');
+				setMessage('');
+			}
+		});
+	};
 
 	return (
 		<ContactFormWrapper>
 			<p>Send a Project Request</p>
-			<Form >
+			<Form>
 				<FormInputContainer>
-					<Input type='text' name='name' placeholder='Name'  />
+					<label htmlFor='name'>Name</label>
+					<Input
+						type='text'
+						onChange={(e) => {
+							setName(e.target.value);
+						}}
+						name='name'
+						placeholder='Name'
+					/>
 				</FormInputContainer>
 				<FormInputContainer>
-					<Input type='text' name='email' placeholder='Email'  />
+					<label htmlFor='email'>Email</label>
+					<Input
+						type='text'
+						onChange={(e) => {
+							setEmail(e.target.value);
+						}}
+						name='email'
+						placeholder='Email'
+					/>
 				</FormInputContainer>
 				<FormInputContainer>
-					<Input type='text' name='phone' placeholder='Phone'  />
+					<label htmlFor='phone'>Phone</label>
+					<Input
+						type='text'
+						onChange={(e) => {
+							setPhone(e.target.value);
+						}}
+						name='phone'
+						placeholder='Phone'
+					/>
 				</FormInputContainer>
 				<MessageTitle>Send A Message</MessageTitle>
 				<FormInputContainer>
-					<InputMessage type='text' name='message' />
+					<label htmlFor='message'>Message</label>
+					<InputMessage
+						type='text'
+						onChange={(e) => {
+							setMessage(e.target.value);
+						}}
+						name='message'
+					/>
 				</FormInputContainer>
 				<InputSubmitContainer>
-					<InputSubmit type='submit' />
+					<InputSubmit
+						type='submit'
+						onClick={(e) => {
+							handleSubmit(e);
+						}}
+					/>
 				</InputSubmitContainer>
 			</Form>
 		</ContactFormWrapper>
---
+++ pages/api/contact.js
@@ -0,0 +1,27 @@
+export default function (req, res) {
+	require('dotenv').config();
+
+	let nodemailer = require('nodemailer');
+	const transporter = nodemailer.createTransport({
+		port: 465,
+		host: 'smtp.gmail.com',
+		auth: {
+			user: 'thomascaldwell11111@gmail.com',
+			pass: process.env.password,
+		},
+		secure: true,
+	});
+	const mailData = {
+		from: 'thomascaldwell11111@gmail.com',
+		to: 'thomasjfcaldwell@gmail.com',
+		subject: `Message From ${req.body.name}`,
+		text: req.body.message + ' | Sent from: ' + req.body.email,
+		html: `<div>${req.body.message}</div><p>Sent from:
+    ${req.body.email}</p>`,
+	};
+	transporter.sendMail(mailData, function (err, info) {
+		if (err) console.log(err);
+		else console.log(info);
+	});
+	res.status(200);
+}
