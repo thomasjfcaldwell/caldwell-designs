ff8e74a31f882fe989574b91bbcbdcd5df35fc6c
thomasjfcaldwell <thomasjfcaldwell@gmail.com>
2021-12-17 09:02:11 -0800

    adding skills page

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -56,66 +56,36 @@ function ContactForm() {
 				<Header>Send a Project Request</Header>
 			</HeaderContainer> */}
 
-			<Form action='get'>
+			<Form
+				action='https://docs.google.com/forms/u/0/d/e/1FAIpQLSdYWaw0WkN_qpVogvapyKaZqx9A_hqGmmMWrWsYIPSziWcBRg/formResponse'
+				method='get'>
 				<FormInputContainer>
-					<Label id='name' htmlFor='name'>
+					<Label id='entry.1673834915' htmlFor='name'>
 						Name
 					</Label>
-					<Input
-						type='text'
-						onChange={(e) => {
-							setName(e.target.value);
-						}}
-						name='name'
-						placeholder='Name'
-					/>
+					<Input type='text' name='entry.1673834915' placeholder='Name' />
 				</FormInputContainer>
 				<FormInputContainer>
-					<Label id='email' htmlFor='email'>
+					<Label name='entry.218700094' htmlFor='email'>
 						Email
 					</Label>
-					<Input
-						type='text'
-						onChange={(e) => {
-							setEmail(e.target.value);
-						}}
-						name='email'
-						placeholder='Email'
-					/>
+					<Input type='text' name='entry.218700094' placeholder='Email' />
 				</FormInputContainer>
 				<FormInputContainer>
-					<Label id='phone' htmlFor='phone'>
+					<Label name='entry.1613609491' htmlFor='phone'>
 						Phone
 					</Label>
-					<Input
-						type='text'
-						onChange={(e) => {
-							setPhone(e.target.value);
-						}}
-						name='phone'
-						placeholder='Phone'
-					/>
+					<Input type='text' name='entry.1613609491' placeholder='Phone' />
 				</FormInputContainer>
 				<MessageTitle>Send A Message</MessageTitle>
 				<FormInputContainer>
-					<Label id='message' htmlFor='message'>
+					<Label name='entry.1359360774' htmlFor='message'>
 						Message
 					</Label>
-					<InputMessage
-						type='text'
-						onChange={(e) => {
-							setMessage(e.target.value);
-						}}
-						name='message'
-					/>
+					<InputMessage type='text' name='entry.1359360774' />
 				</FormInputContainer>
 				<InputSubmitContainer>
-					<InputSubmit
-						type='submit'
-						onClick={(e) => {
-							handleSubmit(e);
-						}}
-					/>
+					<InputSubmit type='submit' />
 				</InputSubmitContainer>
 			</Form>
 		</ContactFormWrapper>
