87050ecec699504cac7bd56dd4d0a27681374d36
thomasjfcaldwell <thomasjfcaldwell@gmail.com>
2022-06-09 13:29:20 -0700

    lastest fixed on projects issue

--- components/Hero/Projects/ProjectsStyles.js
+++ components/Hero/Projects/ProjectsStyles.js
@@ -80,7 +80,7 @@ export const ProjectsText = styled.p`
 	font-weight: 400;
 	grid-area: ${(props) => (props.primary ? '2/1/3/2' : '2/2/3/3')};
 	@media ${(props) => props.theme.breakpoints.xs} {
-		font-size: 0.65;
+		font-size: 8rem;
 		line-height: 1.5;
 		padding-inline: 0.2rem;
 		background-color: red;
